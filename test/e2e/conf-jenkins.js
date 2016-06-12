634ed624b3d8b291566dbaa492d6809d11dfe667
marcin krawczyk <marcinn.krawczyk@gmail.com>
2016-06-12 15:31:10 +0200

    fixed tests

--- test/e2e/conf-jenkins.js
+++ test/e2e/conf-jenkins.js
@@ -14,7 +14,7 @@ exports.config = {
     },
     directConnect: true,
     specs: ['specs/**/*spec.js'],
-    baseUrl: 'http://40.115.23.130/admin',
+    baseUrl: 'http://40.115.23.130/admin/',
     suites: {
         login: 'specs/login/*.spec.js'
     },
