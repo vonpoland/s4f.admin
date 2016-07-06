265f7f64306cf236043c089f00fb2387a903fb4b
marcin <mkrawczyk@gmail.com>
2016-07-06 22:02:24 +0200

    added conf

---
+++ test/e2e/conf-jenkins-production.js
@@ -0,0 +1,32 @@
+/* global jasmine, browser */
+
+exports.config = {
+    onPrepare: function () {
+        browser.driver.manage().window().setSize(1600, 1000);
+        var reporters = require('jasmine-reporters');
+        var junitReporter = new reporters.JUnitXmlReporter({
+            savePath: './e2etests',
+            consolidateAll: true
+        });
+        jasmine.getEnv().addReporter(junitReporter);
+    },
+    capabilities: {
+        browserName: 'chrome'
+    },
+    directConnect: false,
+    specs: ['specs/**/*spec.js'],
+    baseUrl: 'http://test.glosuj.mobi/admin/',
+    suites: {
+        login: 'specs/login/*.spec.js'
+    },
+    params: {
+        login: {
+            user: 'test@s4f.pl',
+            password: 'test',
+            url: '/admin/login'
+        },
+        interactions: {
+            url: '/admin/interaction'
+        }
+    }
+};
