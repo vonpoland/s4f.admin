872d8141421c691eabe5d2def6e45a70f3307f05
marcin <mkrawczyk@gmail.com>
2016-07-19 20:05:24 +0200

    fixed shipitfile

--- shipitfile.js
+++ shipitfile.js
@@ -1,8 +1,12 @@
 module.exports = function (shipit) {
     require('shipit-deploy')(shipit);
 
+   // shipit.task('deployAndInstall', ['deploy'], function () {
+   //     return shipit.remote('cd ' + shipit.config.deployTo + '/current && npm install && npm run jspm install && npm run gulp && passenger-config restart-app ' + shipit.config.deployTo + '/current')
+   // });
+
     shipit.task('deployAndInstall', ['deploy'], function () {
-        return shipit.remote('cd ' + shipit.config.deployTo + '/current && npm install && npm run jspm install && npm run gulp && passenger-config restart-app ' + shipit.config.deployTo + '/current')
+        return shipit.remote('cd ' + shipit.config.deployTo + '/current && npm install && npm run jspm install && npm run gulp')
     });
 
     shipit.initConfig({
@@ -13,12 +17,19 @@ module.exports = function (shipit) {
             ignores: ['.git', 'node_modules'],
             keepReleases: 2,
             deleteOnRollback: false,
-            key: '/home/jenkins/.ssh/key.ppk',
+            key: '/var/jenkins/.ssh/key.ppk',
             shallowClone: true
         },
         dev: {
             branch: 'master',
             servers: 'screen4fans@40.115.23.130'
+        },
+        production: {
+            workspace: '/var/jenkins_home/deploy-tmp',
+            deployTo: '/home/mkrawczyk/domains/screen4fans.com/bigscree-admin',
+            branch: 'master',
+            servers: 'mkrawczyk@screen4fans'
+            key: '/var/jenkins_home/.ssh/prod_key.ppk',
         }
     });
 };
