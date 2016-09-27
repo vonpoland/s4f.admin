952599f85423bc21a51aed05084846656d3b240d
marcin <marcinn.krawczyk@gmail.com>
2016-09-27 20:02:40 +0200

    shipit fix

--- shipitfile.js
+++ shipitfile.js
@@ -1,12 +1,28 @@
 module.exports = function (shipit) {
     require('shipit-deploy')(shipit);
 
-    shipit.task('deployAndInstall', ['deploy'], function () {
+    shipit.task('clean-pm2', function () {
+        return shipit.remote('node ' + shipit.config.pm2Path + ' delete admin')
+    });
+
+    shipit.task('start-pm2', function () {
+        return shipit.remote('cd ' + shipit.config.deployTo + '/current && NODE_ENV=production node'  + shipit.config.pm2Path + ' start --name admin ' + shipit.config.deployTo + '/current/app.js')
+    });
+
+    shipit.task('test', function () {
+        return shipit.remote('pwd')
+    });
+
+    shipit.task('install-dependencies', function () {
         return shipit.remote('NODE_ENV=production cd ' + shipit.config.deployTo + '/current && npm install && npm run jspm install && npm run gulp')
+    });
+
+    shipit.task('deployAndInstall', ['deploy', 'clean-pm2', 'install-dependencies', 'start-pm2'], function () {
     });
 
     shipit.initConfig({
         default: {
+            pm2Path: '/home/mkrawczyk/domains/screen4fans.com/pm2/node_modules/pm2/bin/pm2',
             deployTo: '/var/www/screen4fans-admin',
             repositoryUrl: 'ssh://git@bitbucket.org/vonpo/bigscreen-admin.git',
             workspace: '/var/lib/jenkins/deploy-tmp',
@@ -34,6 +50,11 @@ module.exports = function (shipit) {
             branch: 'master',
             servers: 'marcin@192.168.1.146',
             key: '/m/dev/key/key'
+        },
+        test2: {
+            deployTo: '/home/mkrawczyk/domains/screen4fans.com/bigscreen-admin',
+            workspace: '/m/dev/temp-sync/test', // this is on local
+            servers: 'mkrawczyk@screen4fans.com',
         }
     });
 };
